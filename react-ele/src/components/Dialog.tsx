c33840bd85b159dc55fa637d7f933d5a769d2734
GuoBin <guobin201314@gmail.com>
2019-09-13 11:39:01 +0800

    Dialog组件

--- react-ele/src/components/Dialog.tsx
+++ react-ele/src/components/Dialog.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import './Dialog.css';
+import { Ratings, Size } from "./ratings/Ratings";
 
 export interface DialogProps {
   onClose: (b: boolean) => void;
 }
 
 export class Dialog extends React.Component<DialogProps> {
-
+  score = 4.5;
+  size: Size = 48;
   render() {
     return (
         <div className="full">
           <div className="dialog-warp">
             <div className="main">
-              <h1>main</h1>
-              <h1>main</h1>
+              <h1>星级评价</h1>
+             <Ratings score={this.score} size={this.size}/>
             </div>
           </div>
           <div className="close">
